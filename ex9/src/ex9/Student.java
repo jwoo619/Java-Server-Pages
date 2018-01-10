09011f3d536470ffbed999013b95896143069975
Lee Jin Woo <jwoo619@naver.com>
2018-01-10 20:44:33 +0900

    day9
    bean

---
+++ ex9/src/ex9/Student.java
@@ -0,0 +1,46 @@
+package ex9;
+
+public class Student {
+	
+	
+  private String name;
+  private int age;
+  private int grade;
+  private int num;
+  
+  public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public int getAge() {
+	return age;
+}
+
+public void setAge(int age) {
+	this.age = age;
+}
+
+public int getGrade() {
+	return grade;
+}
+
+public void setGrade(int grade) {
+	this.grade = grade;
+}
+
+public int getNum() {
+	return num;
+}
+
+public void setNum(int num) {
+	this.num = num;
+}
+
+public Student(){
+	  
+  }
+}
